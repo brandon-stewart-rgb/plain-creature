130d086c947b5cd812349396d05cc21fa841ec42
brandon-stewart-rgb <brandon@pixeltree.us>
2021-10-13 13:31:08 -0600

    user auth

--- controllers/api/users-routes.js
+++ controllers/api/users-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Users, Posts, Comments } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, (req, res)=> {
+router.get('/',  (req, res)=> {
     Users.findAll({
         attributes: { exclude: ['password'] }
     })
